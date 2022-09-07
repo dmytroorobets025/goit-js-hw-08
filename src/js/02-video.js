07aa3ec150521f44c23785829dc772bfa8dcc78d
Dmytro Vorobets <dmytrovorobets025@gmail.com>
2022-09-07 16:22:22 +0300

    js-hw-8. 2-3-task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
 
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const PLAYER__KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+    localStorage.setItem(PLAYER__KEY, seconds);
+
+}
+
+const savedTime = Number(localStorage.getItem(PLAYER__KEY));
+
+if (savedTime) {
+    player.setCurrentTime(savedTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
 
+import throttle from 'lodash.throttle';
+
+const FORM__KEY = 'feedback-form-state';
+let formData = {};
+const refs = {
+    inputFormFeedback: document.querySelector('.feedback-form'),
+    inputFormEmail: document.querySelector('[type="email"]'),
+    inputFormMessage: document.querySelector('[name="message"]'),
+    inputFormSubmitBtn: document.querySelector('[type="submit"]'),
+};
+populateTextarea();
+refs.inputFormFeedback.addEventListener('submit', onFormSubmit);
+refs.inputFormFeedback.addEventListener('input', throttle(onInputStorage, 500));
+
+function onInputStorage(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(FORM__KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+
+    console.log(formData);
+    evt.preventDefault();
+
+    const formElements = evt.currentTarget.elements;
+    const emailValue = formElements.email.value;
+    const messageValue = formElements.message.value;
+    if (emailValue === '' || messageValue === '') {
+        alert('Усі поля введення мають бути заповнені!');
+    };
+
+
+    evt.currentTarget.reset();
+    localStorage.removeItem(FORM__KEY);
+    formData = {};
+};
+
+function populateTextarea() {
+
+    const savedStorageInputs = localStorage.getItem(FORM__KEY)
+
+    if (savedStorageInputs) {
+        formData = JSON.parse(savedStorageInputs);
+        refs.inputFormEmail.value = formData.email || '';
+        refs.inputFormMessage.value = formData.message || '';
+
+    }
+}
